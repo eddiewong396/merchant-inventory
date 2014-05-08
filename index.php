d7ef762b4dbb25a02a8fe29b824567e3e3431912
Eddie Wong <isp396@gmail.com>
2014-05-08 16:22:12 +0800

    Edited the index.php

--- index.php
+++ index.php
@@ -1,26 +1,42 @@
 <!DOCTYPE HTML>
 
-<html>
-	<head><title></title></head>
-	
+<html lang="en">
+	<head>
+
+		<meta charset="utf-8">
+
+		<title>The HTML5 Herald</title>
+		<meta name="description" content="The HTML5 Herald">
+		<meta name="author" content="SitePoint">
+
+		<link rel="stylesheet" href="css/styles.css?v=1.0">
+
+		<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+
 	<body>
+
 		<div>
-		<label> Username :
-		<input type="text" 
-				name="username" 
-				id="username" 
-				placeholder="Enter username here" 
-				autofocus="autofocus" >
-		</label>
-		<br />
-		<label> Password :
-		<input type="password" 
-				name="password" 
-				id="password" >
-		</label>
+			<label> Username :
+				<input type="text" 
+					name="username" 
+					id="username" 
+					placeholder="Enter username here" 
+					autofocus="autofocus" >
+			</label>
+			<br />
+			<label> Password :
+				<input type="password" 
+					name="password" 
+					id="password" >
+			</label>
 		</div>
 		<br />
 		<button type="button">Login</button>
 	</body>
 
 </html>
+
+
