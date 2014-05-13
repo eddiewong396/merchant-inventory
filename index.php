f435d8aeb2e8b31c6373a11d1d2125e9f3cb5b48
Eddie Wong <isp396@gmail.com>
2014-05-13 16:13:34 +0800

    Added tags in index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE HTML>
 
-<html lang="en">
-	<head>
+<html>
 
-		<meta charset="utf-8">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
 		<title>Login Page</title>
 		<meta name="description" content="The HTML5 Herald">
@@ -16,9 +16,19 @@
 		<![endif]-->
 	</head>
 
-	<body>
+<body>
 
-		<div>
+	<header>
+		<nav>
+			<ul>
+				<li>Your menu</li>
+			</ul>
+		</nav>
+	</header>
+	
+	<section>
+		<article>
+			<div>
 			<label> Username :
 				<input type="text" 
 					name="username" 
@@ -35,8 +45,35 @@
 		</div>
 		<br />
 		<button type="button">Login</button>
-	</body>
+		</article>
+		
+		<article>
+			<header>
+				<h2>Article title</h2>
+				<p>Posted on <time datetime="2009-09-04T16:31:24+02:00">September 4th 2009</time> by <a href="#">Writer</a> - <a href="#comments">6 comments</a></p>
+			</header>
+			<p>Content 01.</p>
+		</article>
+		
+		<article>
+			<header>
+				<h2>Article title</h2>
+				<p>Posted on <time datetime="2009-09-04T16:31:24+02:00">September 4th 2009</time> by <a href="#">Writer</a> - <a href="#comments">6 comments</a></p>
+			</header>
+			<p>Content 02.</p>
+		</article>
+		
+	</section>
+
+	<aside>
+		<h2>About section</h2>
+		<p>Put something here.</p>
+	</aside>
+
+	<footer>
+		<p>Copyright 2014</p>
+	</footer>
+
+</body>
 
 </html>
-
-
